4e1dca6854615494d7abfd606b058c207b76f351
Ian Ross <126489537+ian-j-ross@users.noreply.github.com>
2023-04-04 23:16:04 +0100

    needed to change header as well

--- rsa.h
+++ rsa.h
@@ -12,16 +12,16 @@
 
 // 5 is number of Nodes, 16 is length of IP, possibly could just store the last 3 digits of ip for less storage/lookup time
 extern char Node_IPs[NUM_NODES][16];
-extern char Node_PubKeys[NUM_NODES][RSA_KEYLEN];
+extern char *Node_PubKeys[NUM_NODES];
 
 //-------------------------------
 //  Digital Signature Functions
 //-------------------------------
 
-void rsaGetPubKey(char *IP, mpz_t *publicKey);          // Input an ip, publicKey[] will return null if user isnt in the network, or a key if they are
+char **rsaGetPubKey(char *IP);                          // Input an ip, publicKey[] will return null if user isnt in the network, or a key if they are
 int verifySig(char *IP, char *rawMsg, char *signature); // Pass IP of sender, RAW message, and signature of sender, Will return 1 if match 0 if false.
 
-char *rsaEncrypt(char *hashedInput, char *privKey, char *pubKey); // inputMsg should be 256bit hash, priv key and output will be in encrypted msg
-char *rsaDecrypt(char *encryptedInput, char *pubKey);             // input msg to decrypt, public key and output will be in decrypted msg
+char *rsaEncrypt(char *hashedInput, char *privKey, char *pubKey);
+char *rsaDecrypt(char *encryptedInput, char *pubKey);
 
 #endif
